683be1f28dbac9b5d373abe1a4695067c7586ab8
happysalada <raphael.megzari@gmail.com>
2018-04-14 19:39:22 +0900

    change token owner

--- smart-contracts/nex/token.py
+++ smart-contracts/nex/token.py
@@ -12,7 +12,7 @@ TOKEN_DECIMALS = 8
 
 # This is the script hash of the address for the owner of the token
 # This can be found in ``neo-python`` with the walet open, use ``wallet`` command
-TOKEN_OWNER = b'S\xefB\xc8\xdf!^\xbeZ|z\xe8\x01\xcb\xc3\xac/\xacI)'
+TOKEN_OWNER = b'#\xba\'\x03\xc52c\xe8\xd6\xe5"\xdc2 39\xdc\xd8\xee\xe9'
 
 TOKEN_CIRC_KEY = b'in_circulation'
 
@@ -25,12 +25,6 @@ TOKENS_PER_NEO = 40 * 100000000
 
 # for now assume 1 dollar per token, and one gas = 20 dollars * 10^8
 TOKENS_PER_GAS = 20 * 100000000
-
-# maximum amount you can mint in the limited round ( 500 neo/person * 40 Tokens/NEO * 10^8 )
-MAX_EXCHANGE_LIMITED_ROUND = 500 * 40 * 100000000
-
-KYC_KEY = b'kyc_ok'
-
 
 def add_to_circulation(ctx, amount):
     """
